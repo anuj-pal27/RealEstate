02781c5c4689db34a0db08177de93dd7a682fe01
anuj-pal27 <anujpal27669@gmail.com>
2024-12-28 21:43:51 +0530

    model and env created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGODB_URL)
+  .then(() => console.log('Connected!').catch(()=>{console.log("error while connection database")}));
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
+
