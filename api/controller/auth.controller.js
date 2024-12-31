567dd7dcac7a382fc336c8f9e92bde90bd39c3c2
anuj-pal27 <anujpal27669@gmail.com>
2024-12-31 17:35:15 +0530

    added signin functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,10 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from '../utils/error.js';
+import jwt from  'jsonwebtoken'
+import 'dotenv/config'
 
-const Signup = async (req,res,next)=>{
+export const signup = async (req,res,next)=>{
     try{
         const {username,password,email} = req.body;
         const hashedPassword = await bcryptjs.hash(password,10);
@@ -18,4 +21,27 @@ const Signup = async (req,res,next)=>{
         next(err);
     }
 }
-export default Signup;
+
+export const signin = async (req,res,next)=>{
+    const {email, password} = req.body;
+    try{
+        const validUser =await User.findOne({email});
+        if(!validUser)
+            return next(errorHandler(404,'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong credentials'));
+        const payload = {
+            id:validUser._id,
+            username:validUser.username,
+            email:validUser.email,
+        }
+        const option = {
+            expiresIn: '1h',
+        }
+        const token = jwt.sign(payload,process.env.JWT_SECRET,option);
+        res.cookie('access_token',token,{httpOnly: true}).status(200).json(validUser);
+
+    }catch(error){
+        next(error);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from 'express' ;
 const router = express.Router();
-import Signup from '../controller/auth.controller.js'
+import {signup,signin} from '../controller/auth.controller.js'
 
-router.post("/signup",Signup);
-
+router.post("/signup",signup);
+router.post("/signin",signin)
 export default router;
